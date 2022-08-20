335485af79210d60cc281dc189a0fd967d056fc9
leodeleonkc <106038050+leodeleonkc@users.noreply.github.com>
2022-08-19 19:06:22 -0500

    fixed for of loop bug
    for-of loop wasn't looping past the first iteration, so I had to deconstruct the returning object into an array and that solved the issue.

--- brains.js
+++ brains.js
@@ -15,8 +15,8 @@ searchButton.addEventListener("click", function (e) {
   findMovie();
 });
 
-// Object from initial fetch
-let movies = {};
+// Array of search results from initial fetch (empty for now)
+let movies;
 
 async function findMovie() {
   // Fetch the initial search query
@@ -24,8 +24,8 @@ async function findMovie() {
     `http://www.omdbapi.com/?apikey=98a9f83d&s=${searchQuery.value}`
   );
   const firstData = await res.json();
-  console.log(firstData);
-  movies = Object.values(firstData);
+  // console.log(firstData);
+  movies = Object.values(firstData.Search);
 
   // Invalid response action
   if (firstData.Response === "True") {
@@ -38,50 +38,57 @@ async function findMovie() {
 }
 
 async function Render() {
-  for (let [{ imdbID }] of movies) {
+  dynamicHtml.innerHTML = "";
+  for (let { imdbID } of movies) {
     const res = await fetch(
       `http://www.omdbapi.com/?apikey=98a9f83d&i=${imdbID}`
     );
     const data = await res.json();
 
-    console.log(data.imdbID);
+    console.log(data.Type);
+
+    // Filter results to only show "movies" (vs series or games)
+    // function isMovie (data) {
+    //   return data.Type = "movie"
+    // }
 
     // Render HTML elements
+
     exploreImg.classList.add("hidden");
     invalidSearch.classList.add("hidden");
     dynamicHtml.innerHTML += `
-        <article class="row" id="content">
-        <div class="movie-poster col-md-3">
-          <img
-            src="${data.Poster}"
-            alt="Movie Poster"
-            class="movie-poster-img"
-          />
-        </div>
-        <div class="col-md-8">
-          <div class="movie-title-and-rating">
-            <h2 class="movie-name">
-              ${data.Title} <span class="rated">${data.Rated}</span>
-            </h2>
-          </div>
-          <div class="movie-small-deets movie-deets">
-            <p class="movie-deets rating">⭐️ ${data.imdbRating}</p>
-            <p class="movie-duration">${data.Runtime}</p>
-            <p class="movie-category"> ${data.Genre} </p>
-            <p class="watchlist">
-              <strong
-                ><i class="bi bi-plus-circle-fill"></i> Watchlist</strong
-              >
-            </p>
-          </div>
-          <div class="movie-description">
-            <p class="movie-info-text">
-              ${data.Plot}
-            </p>
-          </div>
-          <hr>
-        </div>
-      </article>
-        `;
+            <article class="row" id="content">
+            <div class="movie-poster col-md-3">
+              <img
+                src="${data.Poster}"
+                alt="Movie Poster"
+                class="movie-poster-img"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="movie-title-and-rating">
+                <h2 class="movie-name">
+                  ${data.Title} <span class="rated">${data.Rated}</span>
+                </h2>
+              </div>
+              <div class="movie-small-deets movie-deets">
+                <p class="movie-deets rating">⭐️ ${data.imdbRating}</p>
+                <p class="movie-duration">${data.Runtime}</p>
+                <p class="movie-category"> ${data.Genre} </p>
+                <p class="watchlist">
+                  <strong
+                    ><i class="bi bi-plus-circle-fill"></i> Watchlist</strong
+                  >
+                </p>
+              </div>
+              <div class="movie-description">
+                <p class="movie-info-text">
+                  ${data.Plot}
+                </p>
+              </div>
+              <hr>
+            </div>
+          </article>
+            `;
   }
 }
